fc2f20b1f89743b87d9366f9113348c03c9dc2d1
JDM-Github <jdmaster888@gmail.com>
2024-12-12 19:05:59 +0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import csv
+# import csv
 import re
 from collections import defaultdict
 
 # import joblib
-from kivy.config import Config
+# from kivy.config import Config
 
-WIDTH  = int(750  * 0.5) 
-HEIGHT = int(1400 * 0.5) 
-Config.set('graphics', 'width', WIDTH)
-Config.set('graphics', 'height', HEIGHT)
-Config.set('graphics', 'resizable', 0)
-Config.write()
+# WIDTH  = int(750  * 0.5) 
+# HEIGHT = int(1400 * 0.5) 
+# Config.set('graphics', 'width', WIDTH)
+# Config.set('graphics', 'height', HEIGHT)
+# Config.set('graphics', 'resizable', 0)
+# Config.write()
 
-from kivy.utils import platform
+# from kivy.utils import platform
 from kivy.core.window import Window
 
-if platform == "win":
-	Window.size  = (WIDTH, HEIGHT)
-	Window.top   = 30
-	Window.left  = 1
+# if platform == "win":
+# 	Window.size  = (WIDTH, HEIGHT)
+# 	Window.top   = 30
+# 	Window.left  = 1
 
 from kivymd.app import App
 from kivy.app import App
@@ -114,32 +114,32 @@ class MainWidget(Widget):
 
         return category_map
 
-    def process_csv_to_3d_list(self, csv_file):
-        result = []
+    # def process_csv_to_3d_list(self, csv_file):
+    #     result = []
 
-        with open(csv_file, 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            header = next(reader)  
-            category_map = self.categorize_columns(header)
+    #     with open(csv_file, 'r', encoding='utf-8') as file:
+    #         reader = csv.reader(file)
+    #         header = next(reader)  
+    #         category_map = self.categorize_columns(header)
 
-            for row in reader:
-                row_index = 4 
-                for category, subcategories in category_map.items():
-                    category_data = next((item for item in result if item["category"] == category), None)
-                    if not category_data:
-                        category_data = {"category": category, "subcategories": []}
-                        result.append(category_data)
+    #         for row in reader:
+    #             row_index = 4 
+    #             for category, subcategories in category_map.items():
+    #                 category_data = next((item for item in result if item["category"] == category), None)
+    #                 if not category_data:
+    #                     category_data = {"category": category, "subcategories": []}
+    #                     result.append(category_data)
 
-                    for subcat in subcategories:
-                        if "Feedback" not in category:  
-                            stars = int(row[row_index]) 
-                            category_data["subcategories"].append({"subcategory": subcat, "stars": stars})
-                        else:
-                            feedback = row[row_index]
-                            category_data["subcategories"].append({"subcategory": subcat, "text": feedback})
-                        row_index += 1
+    #                 for subcat in subcategories:
+    #                     if "Feedback" not in category:  
+    #                         stars = int(row[row_index]) 
+    #                         category_data["subcategories"].append({"subcategory": subcat, "stars": stars})
+    #                     else:
+    #                         feedback = row[row_index]
+    #                         category_data["subcategories"].append({"subcategory": subcat, "text": feedback})
+    #                     row_index += 1
 
-        return result
+    #     return result
 
     def aggregate_subcategories(self, subcategories):
         """Group subcategories and aggregate stars."""
@@ -159,144 +159,144 @@ class MainWidget(Widget):
         ]
         return result
 
-    def open_csv(self):
-        if not self.full_comments_file:
-            self.show_error_popup("Invalid Submit", "File is missing.")
-            print("INVALID SUBMIT")
-            return
+    # def open_csv(self):
+    #     if not self.full_comments_file:
+    #         self.show_error_popup("Invalid Submit", "File is missing.")
+    #         print("INVALID SUBMIT")
+    #         return
 
-        # model = joblib.load('naive_bayes_model.joblib')
-        # vectorizer = joblib.load('tfidf_vectorizer.joblib')
-        # encoder = joblib.load('label_encoder.joblib')
-        model = None
-        vectorizer = None
-        encoder = None
-        try:
-            with open(r"C:\JDM\SE\Copy of SSC Events' Evaluation Responses - CSL General Assembly 2024.csv", 'r', encoding='utf-8') as file:
-                reader = csv.DictReader(file)
-                full_df = [row for row in reader]
-        except Exception as e:
-            self.show_error_popup("File Read Error", f"Error reading the file: {e}")
-            return
+    #     # model = joblib.load('naive_bayes_model.joblib')
+    #     # vectorizer = joblib.load('tfidf_vectorizer.joblib')
+    #     # encoder = joblib.load('label_encoder.joblib')
+    #     model = None
+    #     vectorizer = None
+    #     encoder = None
+    #     try:
+    #         with open(r"C:\JDM\SE\Copy of SSC Events' Evaluation Responses - CSL General Assembly 2024.csv", 'r', encoding='utf-8') as file:
+    #             reader = csv.DictReader(file)
+    #             full_df = [row for row in reader]
+    #     except Exception as e:
+    #         self.show_error_popup("File Read Error", f"Error reading the file: {e}")
+    #         return
     
-        self.current_index = 0
-        self.sub_current_index = 0
-        self.structured_data = self.process_csv_to_3d_list(self.full_comments_file)
+    #     self.current_index = 0
+    #     self.sub_current_index = 0
+    #     self.structured_data = self.process_csv_to_3d_list(self.full_comments_file)
 
-        if self.structured_data:
-            self.category_name = self.structured_data[self.current_index].get('category', 'Unknown Category')
+    #     if self.structured_data:
+    #         self.category_name = self.structured_data[self.current_index].get('category', 'Unknown Category')
             
-            self.sub_category_list = self.aggregate_subcategories(self.structured_data[self.current_index].get('subcategories', []))
+    #         self.sub_category_list = self.aggregate_subcategories(self.structured_data[self.current_index].get('subcategories', []))
             
-            if self.sub_category_list:
-                data = self.sub_category_list[self.sub_current_index]
-                total_votes = sum(data['stars'])
-                star_percentages = [(votes / total_votes) * 100 if total_votes > 0 else 0 for votes in data['stars']]
+    #         if self.sub_category_list:
+    #             data = self.sub_category_list[self.sub_current_index]
+    #             total_votes = sum(data['stars'])
+    #             star_percentages = [(votes / total_votes) * 100 if total_votes > 0 else 0 for votes in data['stars']]
                 
-                self.subcategory_name = re.sub(r'^\d+\.\s*', '', data['subcategory'])
-                self.subcategory_star = star_percentages
+    #             self.subcategory_name = re.sub(r'^\d+\.\s*', '', data['subcategory'])
+    #             self.subcategory_star = star_percentages
 
-                self.number_star_1 = f"{data['stars'][0]}"
-                self.number_star_2 = f"{data['stars'][1]}"
-                self.number_star_3 = f"{data['stars'][2]}"
-                self.number_star_4 = f"{data['stars'][3]}"
-                self.number_star_5 = f"{data['stars'][4]}"
+    #             self.number_star_1 = f"{data['stars'][0]}"
+    #             self.number_star_2 = f"{data['stars'][1]}"
+    #             self.number_star_3 = f"{data['stars'][2]}"
+    #             self.number_star_4 = f"{data['stars'][3]}"
+    #             self.number_star_5 = f"{data['stars'][4]}"
 
-                stars = data['stars']
-                total_votes = sum(stars)
+    #             stars = data['stars']
+    #             total_votes = sum(stars)
 
-                if total_votes > 0:
-                    weighted_average = sum((i + 1) * stars[i] for i in range(5)) / total_votes
-                else:
-                    weighted_average = 0 
+    #             if total_votes > 0:
+    #                 weighted_average = sum((i + 1) * stars[i] for i in range(5)) / total_votes
+    #             else:
+    #                 weighted_average = 0 
 
-                if weighted_average >= 4.8:
-                    self.result_text = "Very Satisfactory"
-                    self.result_color = "#00FF00"
-                elif 3.5 <= weighted_average < 4.8:
-                    self.result_text = "Satisfactory"
-                    self.result_color = "#FFA500"
-                elif 2.5 <= weighted_average < 3.5:
-                    self.result_text = "Needs Improvement"
-                    self.result_color = "#FFCC00"
-                else:
-                    self.result_text = "Needs Enhancement"
-                    self.result_color = "#FF0000" 
+    #             if weighted_average >= 4.8:
+    #                 self.result_text = "Very Satisfactory"
+    #                 self.result_color = "#00FF00"
+    #             elif 3.5 <= weighted_average < 4.8:
+    #                 self.result_text = "Satisfactory"
+    #                 self.result_color = "#FFA500"
+    #             elif 2.5 <= weighted_average < 3.5:
+    #                 self.result_text = "Needs Improvement"
+    #                 self.result_color = "#FFCC00"
+    #             else:
+    #                 self.result_text = "Needs Enhancement"
+    #                 self.result_color = "#FF0000" 
 
-                colors = (237 / 255, 106 / 255, 110 / 255)
-                for i, percentage in enumerate(star_percentages):
-                    star_id = f"star_{i + 1}"
-                    if hasattr(self.ids, star_id):
-                        self.redraw_canvas(getattr(self.ids, star_id), colors, percentage)
-                    else:
-                        print(f"Warning: {star_id} does not exist in self.ids.")
-            else:
-                print("Warning: No subcategories available.")
-        else:
-            print("Warning: No data available in structured data.")
+    #             colors = (237 / 255, 106 / 255, 110 / 255)
+    #             for i, percentage in enumerate(star_percentages):
+    #                 star_id = f"star_{i + 1}"
+    #                 if hasattr(self.ids, star_id):
+    #                     self.redraw_canvas(getattr(self.ids, star_id), colors, percentage)
+    #                 else:
+    #                     print(f"Warning: {star_id} does not exist in self.ids.")
+    #         else:
+    #             print("Warning: No subcategories available.")
+    #     else:
+    #         print("Warning: No data available in structured data.")
 
-        target = None
-        if full_df:
-            for col in ['comment', 'comments', 'review', 'text']:
-                for key in full_df[0].keys():
-                    if col in key.lower():
-                        target = [row[key] for row in full_df]
-                        break
-                if target:
-                    break
+    #     target = None
+    #     if full_df:
+    #         for col in ['comment', 'comments', 'review', 'text']:
+    #             for key in full_df[0].keys():
+    #                 if col in key.lower():
+    #                     target = [row[key] for row in full_df]
+    #                     break
+    #             if target:
+    #                 break
 
-            if target is None:
-                print("Missing Column: The file must contain a 'comment, review, text' column.")
-                return
-        else:
-            print("Missing Column: The file must contain a 'comment, review, text' column.")
-            return
+    #         if target is None:
+    #             print("Missing Column: The file must contain a 'comment, review, text' column.")
+    #             return
+    #     else:
+    #         print("Missing Column: The file must contain a 'comment, review, text' column.")
+    #         return
 
-        predicted_sentiments = []
-        for comment in target:
-            transformed_comment = vectorizer.transform([comment])
-            prediction_probabilities = model.predict_proba(transformed_comment)
+    #     predicted_sentiments = []
+    #     for comment in target:
+    #         transformed_comment = vectorizer.transform([comment])
+    #         prediction_probabilities = model.predict_proba(transformed_comment)
 
-            neutral_prob = prediction_probabilities[0][0] < 0.58 and prediction_probabilities[0][1] < 0.58
+    #         neutral_prob = prediction_probabilities[0][0] < 0.58 and prediction_probabilities[0][1] < 0.58
 
-            if neutral_prob:
-                sentiment = 'Neutral'
-            else:
-                prediction = model.predict(transformed_comment)
-                sentiment = encoder.inverse_transform(prediction)[0]
+    #         if neutral_prob:
+    #             sentiment = 'Neutral'
+    #         else:
+    #             prediction = model.predict(transformed_comment)
+    #             sentiment = encoder.inverse_transform(prediction)[0]
 
-            predicted_sentiments.append(sentiment)
+    #         predicted_sentiments.append(sentiment)
 
-        for i, row in enumerate(full_df):
-            row['Predicted_Sentiment'] = predicted_sentiments[i]
+    #     for i, row in enumerate(full_df):
+    #         row['Predicted_Sentiment'] = predicted_sentiments[i]
 
-        positive_comments = [row for row in full_df if row['Predicted_Sentiment'] == 'Positive']
-        negative_comments = [row for row in full_df if row['Predicted_Sentiment'] == 'Negative']
-        neutral_comments = [row for row in full_df if row['Predicted_Sentiment'] == 'Neutral']
+    #     positive_comments = [row for row in full_df if row['Predicted_Sentiment'] == 'Positive']
+    #     negative_comments = [row for row in full_df if row['Predicted_Sentiment'] == 'Negative']
+    #     neutral_comments = [row for row in full_df if row['Predicted_Sentiment'] == 'Neutral']
 
-        self.positive_len = len(positive_comments)
-        self.negative_len = len(negative_comments)
-        self.neutral_len = len(neutral_comments)
+    #     self.positive_len = len(positive_comments)
+    #     self.negative_len = len(negative_comments)
+    #     self.neutral_len = len(neutral_comments)
 
-        total_comments = len(full_df)
-        self.positive_percent = (self.positive_len / total_comments) * 100 if total_comments else 0
-        self.neutral_percent = (self.neutral_len / total_comments) * 100 if total_comments else 0
-        self.negative_percent = (self.negative_len / total_comments) * 100 if total_comments else 0
+    #     total_comments = len(full_df)
+    #     self.positive_percent = (self.positive_len / total_comments) * 100 if total_comments else 0
+    #     self.neutral_percent = (self.neutral_len / total_comments) * 100 if total_comments else 0
+    #     self.negative_percent = (self.negative_len / total_comments) * 100 if total_comments else 0
 
-        self.redraw_canvas(self.ids.negative_bar, (237 / 255, 106 / 255, 110 / 255), self.negative_percent)
-        self.redraw_canvas(self.ids.neutral_bar, (240 / 255, 137 / 255, 44 / 255), self.neutral_percent)
-        self.redraw_canvas(self.ids.positive_bar, (134 / 255, 207 / 255, 111 / 255), self.positive_percent)
+    #     self.redraw_canvas(self.ids.negative_bar, (237 / 255, 106 / 255, 110 / 255), self.negative_percent)
+    #     self.redraw_canvas(self.ids.neutral_bar, (240 / 255, 137 / 255, 44 / 255), self.neutral_percent)
+    #     self.redraw_canvas(self.ids.positive_bar, (134 / 255, 207 / 255, 111 / 255), self.positive_percent)
 
-        percentages = {
-            "POSITIVE": self.positive_percent,
-            "NEUTRAL": self.neutral_percent,
-            "NEGATIVE": self.negative_percent
-        }
+    #     percentages = {
+    #         "POSITIVE": self.positive_percent,
+    #         "NEUTRAL": self.neutral_percent,
+    #         "NEGATIVE": self.negative_percent
+    #     }
 
-        self.max_percent_label = max(percentages, key=percentages.get)
-        self.max_percent = int(percentages[self.max_percent_label])
+    #     self.max_percent_label = max(percentages, key=percentages.get)
+    #     self.max_percent = int(percentages[self.max_percent_label])
 
-        self.draw_pie_chart([self.negative_percent, self.neutral_percent, self.positive_percent])
+    #     self.draw_pie_chart([self.negative_percent, self.neutral_percent, self.positive_percent])
 
     def prev_subcategory(self):
         if self.sub_current_index > 0:
